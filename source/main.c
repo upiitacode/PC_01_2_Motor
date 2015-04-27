940935f2824061dc6b2a76b78c4dffdff756ed60
Manuel Limones <limonesu.me@gmail.com>
2015-04-27 08:11:40 -0500

    add retarget of type serial_stdio, redirect output to USART1

--- source/main.c
+++ source/main.c
@@ -6,7 +6,8 @@
 
 #include "stm32f30x.h"                  // Device header
 #include "timer2_ch2_pwm.h"
-#include <stdio.h>
+#include "serial_stdio.h"
+#include "retarget_stm32f3.h"
 #include <string.h>
 /*Led PB13, Button PC13*/
 
@@ -14,12 +15,8 @@ void delay_ms(int delay_time);
 
 void DIR_CTRL_Init(void);
 
-void USART2_init(void);
-void USART2_sendChar(char ch);
-char USART2_getChar(void);
+Serial_t USART1_serial = {USART1_getChar, USART1_sendChar};
 
-void s_puts(const char * pString);
-int s_gets(char * pBuffer, int bufferSize);
 char myInBuffer[80];
 char myOutBuffer[80];
 char wordBuffer[80];
@@ -39,18 +36,16 @@ int main(){
 	//Initialize Direction control signals
 	DIR_CTRL_Init();
 	//Initialize USART
-	USART2_init();
-	s_puts("\nSystem ready\n");
+	USART1_init(9600);
+	serial_puts(USART1_serial,"\nSystem ready\n");
 	
 	while(1){
-		s_gets(myInBuffer,80);
+		serial_gets(USART1_serial,myInBuffer,80);
 		tokensFound = sscanf(myInBuffer,"V%d",&newSpeed);
 		if(tokensFound == 1){
-			snprintf(myOutBuffer,80,"new Speed = %d\n",newSpeed);
-			s_puts(myOutBuffer);
+			serial_printf(USART1_serial,"new Speed = %d\n",newSpeed);
 			command_speed=newSpeed;
 		}
-		
 		//Glow the led every 3 seconds aprox
 		while(1){
 			error=command_speed-current_speed;			
@@ -70,7 +65,7 @@ int main(){
 				GPIO_WriteBit(GPIOB,GPIO_Pin_5,Bit_SET);
 			}
 			TIMER2_CH2_PWM_SetDutyCycle(((current_speed>0) ? current_speed : (-current_speed)),myAutorreload);
-			delay_ms(0x4FFF);
+			delay_ms(0x1900);
 		}
 	}
 }
@@ -90,83 +85,6 @@ void DIR_CTRL_Init(void){
 	GPIO_InitStructure.GPIO_PuPd=GPIO_PuPd_UP;
 	GPIO_Init(GPIOB,&GPIO_InitStructure);
 	GPIO_ResetBits(GPIOB, (GPIO_Pin_4 | GPIO_Pin_5));
-}
-
-int s_gets(char * pBuffer, int bufferSize){
-	char newChar; 
-	int i ;
-	for(i = 0 ; i < (bufferSize - 1); i++){
-		newChar = USART2_getChar();
-		if( (newChar != '\r') ){
-			pBuffer[i]=newChar;
-		}else{
-			pBuffer[i]='\0';
-			return i;
-		}
-	}
-	pBuffer[bufferSize-1]='\0';
-	return bufferSize-1;
-}
-
-void s_puts(const char * pString){
-	char newChar; 
-	int i ;
-	for(i = 0 ; i < 81; i++){
-		newChar = pString[i];
-		if( newChar != '\0' ){
-			USART2_sendChar(newChar);
-		}else{
-			break;
-		}
-		
-	}
-}
-
-void USART2_sendChar(char ch){
-	while(!USART_GetFlagStatus(USART2,USART_FLAG_TXE));
-	
-	USART_SendData(USART2,ch);
-}
-
-char USART2_getChar(void){
-	if(USART_GetFlagStatus(USART2,USART_FLAG_ORE)) USART_ClearFlag(USART2,USART_FLAG_ORE); 
-	
-	while(!USART_GetFlagStatus(USART2,USART_FLAG_RXNE));
-	return USART_ReceiveData(USART2);
-}
-
-void USART2_init(void){
-	GPIO_InitTypeDef GPIO_InitStructure;
-	USART_InitTypeDef USART_InitStructure;
-	
-	/*Enable peripheral clock for GPIOA*/
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA,ENABLE);
-	/*Enable peripheral clock for UART2*/
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
-	
-	/*GPIOA Configuration PA2 as TX PA3 as RX*/
-	GPIO_StructInit(&GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_2|GPIO_Pin_3;
-	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_OType=GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd=GPIO_PuPd_UP;
-	GPIO_Init(GPIOA,&GPIO_InitStructure);
-	
-	/*Connect USART2 pins to AF2*/
-	//TX=PA2
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource2,GPIO_AF_7);
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource3,GPIO_AF_7);
-	
-	USART_StructInit(&USART_InitStructure);
-	USART_InitStructure.USART_BaudRate=9600;
-	USART_InitStructure.USART_WordLength=USART_WordLength_8b;
-	USART_InitStructure.USART_StopBits=USART_StopBits_1;
-	USART_InitStructure.USART_Parity=USART_Parity_No;
-	USART_InitStructure.USART_HardwareFlowControl=USART_HardwareFlowControl_None;
-	USART_InitStructure.USART_Mode=USART_Mode_Tx|USART_Mode_Rx;
-	USART_Init(USART2,&USART_InitStructure);
-	USART_Cmd(USART2,ENABLE);
 }
 
 void delay_ms(int delay_time){
